b444fb5026aa3ee73ac0fcd6f983d1dd86a4d0e1
DFmoon <1375709980@qq.com>
2019-04-15 10:04:32 +0800

    Initial commit

---
+++ todolist/src/components/liItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li @click='del'>{{item}}</li>
+</template>
+
+<script>
+export default {
+  props:['item','index'],
+  methods:{
+    del:function(){
+      this.$emit('delete',this.index);		//局部组件向外发布事件
+    }
+  },
+}
+</script>
+
+<!-- scoped限制了该样式只在当前组件中有效，若去掉，则全局有效 -->
+<style scoped>
+
+</style>
---
+++ todolist/src/todolist.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input type="text" v-model="itemValue"/>
+    <button @click="submit">提交</button>
+    <ul>
+      <li-item v-for="(item,index) of list" :item="item" :index="index" @delete="deleteItem"></li-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import liItem from './components/liItem'  //导入局部组件
+
+export default {
+  components: {
+    'li-item':liItem,     //注册局部组件
+  },
+  data:function(){
+    return{
+      itemValue:'',
+      list:[],
+    }
+  },
+  methods:{
+    submit:function(){
+      this.list.push(this.itemValue);
+      this.itemValue='';
+    },
+    deleteItem:function(index){
+      this.list.splice(index,1);
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
